cc8d153fdb2f98beb7eab33183cca46b352e834c
Kurt Wheeler <kurt.wheeler91@gmail.com>
2021-08-11 17:13:21 -0400

    Bump version

--- fancySVD/__init__.py
+++ fancySVD/__init__.py
@@ -3,9 +3,6 @@ from __future__ import absolute_import, division, print_function
 from .iterative_svd import IterativeSVD
 from .solver import Solver
 
-# while iterative imputer is experimental in sklearn, we need this
-
-
 __version__ = "0.1.2"
 
 __all__ = ["IterativeSVD", "Solver"]
