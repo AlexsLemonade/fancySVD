1575238321bf33c7f1c90990036c8c48cbdf3990
Kurt Wheeler <kurt.wheeler91@gmail.com>
2021-08-12 10:09:10 -0400

    Forgot to bump the version in the last commmit.

--- fancySVD/__init__.py
+++ fancySVD/__init__.py
@@ -3,6 +3,6 @@ from __future__ import absolute_import, division, print_function
 from .iterative_svd import IterativeSVD
 from .solver import Solver
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 __all__ = ["IterativeSVD", "Solver"]
